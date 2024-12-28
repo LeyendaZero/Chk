50f28f296c17222fb79dff51d42a0874bae606b5
LeyendaZero <aopenhimer@gmail.com>
2024-12-28 01:17:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ with open("FILES/config.json", "r", encoding="utf-8") as f:
     DATA      = json.load(f)
     API_ID    = DATA["API_ID"]
     API_HASH  = DATA["API_HASH"]
-    BOT_TOKEN = DATA["BOT_TOKEN"]
+    BOT_TOKEN = DATA["7751263364:AAHWaMk90iayycmF89QcEo1gXxk40TIwOLA"]
 
 user = Client( 
             "Scrapper", 
